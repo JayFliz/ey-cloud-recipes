730bdaf1a8c8fd1d061dd8d1a1f7a39f192565cb
JayFliz <jay@fliz.co.uk>
2016-02-20 19:03:17 +0000

    redis

--- cookbooks/redis/attributes/default.rb
+++ cookbooks/redis/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:redis] = {
   :utility_name => "redis",
-  :version => "2.8.24",
+  :version => "2.8.13",
   :bindport => "6379",
   :unixsocket => "/tmp/redis.sock",
   :basename => "dump.rdb",
